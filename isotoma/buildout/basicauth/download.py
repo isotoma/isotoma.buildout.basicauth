d3edb5813d9f20b95db7ef393dc977747b0fbc5f
Alex Holmes <alex@alex-holmes.com>
2012-04-29 19:59:29 +0100

    Move the download logic out into reusable functions

--- isotoma/buildout/basicauth/download.py
+++ isotoma/buildout/basicauth/download.py
@@ -14,29 +14,57 @@ logger = logging.getLogger(__name__)
 
 from zc.buildout import download
 
-def credentials_for_url(credentials, url):
+def credential_for_url(url, credentials):
+    """Given a list of `Credential` objects, find the one pertaining to the
+    relevant URL"""
+
     this_netloc = urlparse.urlparse(url)[1]
     for credential in credentials:
         netloc = urlparse.urlparse(credential.uri)[1]
         if netloc == this_netloc:
-            username, password, realm, url = credential.get_credentials()
-            return username, password
-    return None, None
+            return credential
+
+def _inject_credentials(url, username=None, password=None):
+    """Used by `inject_credentials` decorators to actually do the injecting"""
+
+    if username and password:
+        scheme, netloc, path, params, query, frag = urlparse.urlparse(url)
+        if scheme in ('http', 'https'):
+            auth, host = urllib2.splituser(netloc)
+            if not auth: # If the URL doesn't have credentials in it already
+                netloc = '%s:%s@%s' % (
+                    urllib.quote(username),
+                    urllib.quote(password),
+                    host,
+                )
+                url = urlparse.urlunparse((scheme,netloc,path,params,query,frag))
+    return url
 
 def inject_credentials(credentials):
+    """Decorator factory returning a decorator that will keep injecting the
+    relevant `Credential` into a URL until the `Credential` is exhausted."""
+
     def decorator(auth_func):
         def wrapper(url):
-            logger.critical('Fetching URL %s' % url)
+            logger.info('Fetching URL %s' % url)
+            credential = credential_for_url(url, credentials)
+            if not credential:
+                logger.debug('No credentials for URL %s' % url)
+                return auth_func(url)
 
-            scheme, netloc, path, params, query, frag = urlparse.urlparse(url)
-            if scheme in ('http', 'https'):
-                auth, host = urllib2.splituser(netloc)
-                if not auth:
-                    username, password = credentials_for_url(credentials, url)
-                    if username:
-                        netloc = '%s%s@%s' % (username, password, host)
-                        url = urlparse.urlunparse((scheme,netloc,path,params,query,frag))
-            return auth_func(url)
+            for cred_tuple in credential.get_credentials():
+                try:
+                    new_url = _inject_credentials(url, cred_tuple[0], cred_tuple[1])
+                    return auth_func(new_url)
+                except Exception, e:
+                    code = getattr(e, 'code', 'unknown')
+                    if code == 401:
+                        logger.critical('Credentials for %s failed.' % url)
+                    else:
+                        logger.critical('Cannot fetch %s (%r)' % (url, code))
+
+            # If we still haven't managed to return a value, re-raise
+            if e: raise e
 
         return wrapper
     return decorator
