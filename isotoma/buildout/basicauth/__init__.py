5c76a7016559c1a4b6ee2a458fe0ee44dc996294
Alex Holmes <alex@alex-holmes.com>
2012-04-30 17:37:53 +0100

    Move the documentation into the README

--- isotoma/buildout/basicauth/__init__.py
+++ isotoma/buildout/basicauth/__init__.py
@@ -1,34 +1,4 @@
 """
-==========================
-isotoma.buildout.basicauth
-==========================
-
-We expect a config as follows::
-
-    [basicauth]
-    credentials =
-        pypi
-        github
-    interactive = yes
-
-    [pypi]
-    uri = https://example.com/
-    use-pypirc = pypi
-
-    [github]
-    uri = https://raw.github.com/
-
-In your basicauth part you define each of the credentials parts.
-
-Each of the credentials parts provides authentication details for a different
-uri. The part must contain a uri, and specify what methods to use to obtain
-the other credentials details.
-
-These credential-fetching methods will be tried in order, until all of the
-required credentials have been fetched.
-
-The "interactive" option determines whether or not fetcher methods can prompt
-the user for input.
 """
 
 import os
